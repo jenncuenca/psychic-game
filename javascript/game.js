4e56246b5c8ddd43537248f190c871dd42d49e86
Jennifer Cuenca <>
2018-02-11 13:59:24 -0500

    added img, footer, and updated game coding

--- javascript/game.js
+++ javascript/game.js
@@ -1,48 +1,49 @@
-    
-// Random Letter Stored as Variable
+// Computer chooses random variable/Alphabet Array
+var choices=["a", "b", "c", "d", "e", "f", "g", "h", "i", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-Math.floor(Math.random() * chars.length) +1;
+//Variables to hold the number of wins, losses, and ties. They start at 0.
+var userWins = 0;
+var userLosses = 0;
+var userGuessesLeft=10;
+ 
+// Function for whenever user presses a key.
+document.onkeyup = function(event){
 
-// Prompt User to guess a letter from A-Z
+    var userGuess = event.key; // To determine which key was pressed 
 
-//Check to see if letter is correct
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = choices[Math.floor(Math.random() * choices.length)];
 
 //If number is wrong ask the user to try again. Repeat until correct or User runs out of Guesses.
 
-//If user guess correctly, reveal the correct letter and how many attempts it took.
-
-// Ask if they want to play again.
-
-//Keep track of wins and losses.
-
-
-var randomletter = randomString();
-    var score = "";
-    var loses = "";
-    var attempts = "";
-    var lives = "";
-
-    function randomString() {
-        var chars = "abcdefghiklmnopqrstuvwxyz";
-        var string_length = 1;
-        var randomstring = '';
-        for (var i=0; i<string_length; i++) {
-            var rnum = Math.floor(Math.random() * chars.length);
-            randomstring += chars.substring(rnum,rnum+1);
-        }
-        console.log(randomstring);
-        return(randomstring);
-    }
-
-
-// attempts
-while (attempts != randomLetter){
-    var attempts = prompt ("Please pick a ltter from A-Z")
-    if (counter > maxAttempts) {
-        prompt("You have no more attempts left! Press ok to Play Again")
-    }
-
-    if (attempts === RandomLetter) {
-        prompt("YOU WIN! YOU GUESSED THE CORRECT LETTER!")
-    }
+if (userGuess === computerGuess) { //user wins
+    userWins += 1;
+    userGuessesLeft = 10;
+    guess = '';
+    document.getElementById("wins").innerHTML = "Wins: " + userWins;
+    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
+    document.getElementById("guesses").innerHTML = "Your Guesses so far: ";
 }
+
+else if ((userGuess !== computerGuess) && (userGuessesLeft > 1)) {
+    userGuessesLeft -= 1;  
+    guess = guess + userGuess + ", ";
+    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
+    document.getElementById("guesses").innerHTML = "Your Guesses so far: " + guess;
+}
+
+else { // user loses
+    userLosses+=1;
+    userGuessesLeft = 10;
+    guess = '';
+    document.getElementById("losses").innerHTML = "Losses: " + userLosses;
+    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
+    document.getElementById("guesses").innerHTML = "Your Guesses so far: ";
+}
+
+}
+
+
+
+//If user guess correctly, reveal the correct letter and how many attempts it took.
+// Ask if they want to play again.
