c3fe09cfbaed3611517546bb8b9cce49f9d0692c
Jennifer Cuenca <>
2018-02-11 18:56:41 -0500

    updated javascript - letters guessed resets after user wins the game

--- javascript/game.js
+++ javascript/game.js
@@ -5,22 +5,26 @@ var choices=["a", "b", "c", "d", "e", "f", "g", "h", "i", "k","l", "m", "n", "o"
 var userWins = 0;
 var userLosses = 0;
 var userGuessesLeft=10;
-var guessesMade = [];
+var guessesMade= [];
 
 // Function for whenever user presses a key.
 document.onkeyup = function(event){
-    var userGuess = event.key; // Determine which key was pressed.
-    // User guesses recorded
-     guessesMade.push(event.key);
+    // Determine which key was pressed.
+    var userGuess = event.key; 
     // Computer chooses a letter at random from the choices array.
     var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // User guesses recorded
+    guessesMade.push(event.key);
     
 // if user guess is same as computer choice then user wins (true).
     if (userGuess === computerChoice){
         userWins += 1; // wins increases by 1.
         userGuesses: 10; // guesses user has stays the same.
-        alert("Congratulations! You Guessed Correctly! " + "I was thinking of the Letter: " + computerChoice) // informs user of win
+        alert("Congratulations! You Guessed Correctly! " + "I was thinking of the Letter: " + computerChoice); // informs user of win
+        guessesMade= []; // guesses made get reset
     }
+
+
 
 // if user guess is different then user loses (false).
     else if ((userGuess !== computerChoice) && (userGuessesLeft > 1)) {
@@ -31,13 +35,14 @@ document.onkeyup = function(event){
     else {
         userLosses+=1;
         userGuessesLeft = 10;
+        guessesMade= [];
 }
 
 //Document prints to HTML
 document.getElementById("wins").innerHTML = "Wins: " + userWins; // wins
 document.getElementById("losses").innerHTML = "Losses: " + userLosses; // losses
 document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft; // guesses left
-document.getElementById("guesses").innerHTML = "Your guesses so far: "+ guessesMade // letters guessed
+document.getElementById("guesses").innerHTML = "Your Guesses so far: " + guessesMade;
 
 }
 
