c3a1d8c7488a69fe3bd67c6275f0bb71d33e6b9a
Jennifer Cuenca <>
2018-02-11 17:19:12 -0500

    updated game javascript code. added winning alert.

--- javascript/game.js
+++ javascript/game.js
@@ -1,49 +1,55 @@
-// Computer chooses random variable/Alphabet Array
+// Computer chooses random variable/Alphabet Array.
 var choices=["a", "b", "c", "d", "e", "f", "g", "h", "i", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Variables to hold the number of wins, losses, and ties. They start at 0.
+//Variables to hold the number of wins, losses, and guesses left.
 var userWins = 0;
 var userLosses = 0;
 var userGuessesLeft=10;
- 
+var guessesMade = [''];
+
 // Function for whenever user presses a key.
 document.onkeyup = function(event){
+debugger
+    var userGuess = event.key; // Determine which key was pressed.
 
-    var userGuess = event.key; // To determine which key was pressed 
+    // Computer chooses a letter at random from the choices array.
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    
+// if user guess is same as computer choice then user wins (true).
+    if (userGuess === computerChoice){
+        userWins += 1; // wins increases by 1.
+        userGuesses: 10; // guesses user has stays the same.
+        alert("Congratulations! You Guessed Correctly! " + "I was thinking of the Letter: " + computerChoice) // informs user of win
+    }
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = choices[Math.floor(Math.random() * choices.length)];
-
-//If number is wrong ask the user to try again. Repeat until correct or User runs out of Guesses.
-
-if (userGuess === computerGuess) { //user wins
-    userWins += 1;
-    userGuessesLeft = 10;
-    guess = '';
-    document.getElementById("wins").innerHTML = "Wins: " + userWins;
-    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
-    document.getElementById("guesses").innerHTML = "Your Guesses so far: ";
+// if user guess is different then user loses (false).
+    else if ((userGuess !== computerChoice) && (userGuessesLeft > 1)) {
+    userGuessesLeft -= 1; // guesses left decrease.
+    
 }
 
-else if ((userGuess !== computerGuess) && (userGuessesLeft > 1)) {
-    userGuessesLeft -= 1;  
-    guess = guess + userGuess + ", ";
-    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
-    document.getElementById("guesses").innerHTML = "Your Guesses so far: " + guess;
+// losses recorded and guesses left.
+    else {
+        userLosses+=1;
+        userGuessesLeft = 10;
 }
 
-else { // user loses
-    userLosses+=1;
-    userGuessesLeft = 10;
-    guess = '';
-    document.getElementById("losses").innerHTML = "Losses: " + userLosses;
-    document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft;
-    document.getElementById("guesses").innerHTML = "Your Guesses so far: ";
-}
+// User guesses recorded
+guessesMade.push=(event.key);
+
+//Document prints to HTML
+document.getElementById("wins").innerHTML = "Wins: " + userWins; // wins
+document.getElementById("losses").innerHTML = "Losses: " + userLosses; // losses
+document.getElementById("maxGuesses").innerHTML = "Guesses Left: " + userGuessesLeft; // guesses left
+document.getElementById("guesses").innerHTML = "Your guesses so far: "+ guessesMade; // letters guessed
 
 }
+
+//Game Resets
+
+
+
 
 
 
 //If user guess correctly, reveal the correct letter and how many attempts it took.
-// Ask if they want to play again.
